ff9179292be72e5c19156df63593fbebe329a3c7
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-04-26 21:05:12 -0500

    Stop using LCS

--- lib/main.rb
+++ lib/main.rb
@@ -1,44 +1,8 @@
 require 'clipboard'
 
-class LongestCommonSubstring
-  @@last_input = []
-  @@last_answer = 0
-
-  def self.length_for_pair(a,b)
-    if @@last_input == [a,b]
-      return @@last_answer
-    else
-      @@last_input = [a,b]
-      @@last_answer = compute_length_for_pair(a,b)
-      @@last_answer
-    end
-  end
-
-  #taken from wikipedia
-  def self.compute_length_for_pair(a,b)
-    prefix_length_cache = (0..a.length).map { [] }
-    longest = 0
-    (0...a.length).each do |i|
-      (0...b.length).each do |j|
-        if a[i] == b[j]
-          if i == 0 || j == 0
-            prefix_length_cache[i][j] = 1
-          else
-            prefix_length_cache[i][j] = prefix_length_cache[i-1][j-1] + 1
-          end
-          if prefix_length_cache[i][j] > longest
-            longest = prefix_length_cache[i][j]
-          end
-        else
-          prefix_length_cache[i][j] = 0
-        end
-      end
-    end
-    return longest
-  end
-end
-
 class PrivateKeys < Array
+  KEY_END = "\n-----END RSA PRIVATE KEY-----"
+
   class Filesystem
     def self.all
       [`cat ~/.ssh/id_rsa`]
@@ -54,8 +18,11 @@ class PrivateKeys < Array
 
   def similar?(target)
     any? do |key|
-      lcs_length = LongestCommonSubstring.length_for_pair(key, target)
-      lcs_length >= self.minimum_match_length
+      key_end_index = key.index(KEY_END)
+      if key_end_index
+        last_characters = key[key_end_index - minimum_match_length...key_end_index]
+        target.index(last_characters)
+      end
     end
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,29 +1,12 @@
 require 'main'
 
-describe LongestCommonSubstring do
-  it "finds the longest common substring" do
-    substring = LongestCommonSubstring.length_for_pair("aaHELLObb", "asdfHELLOfoobar")
-    expect(substring).to eq("HELLO".length)
-  end
-
-  it "does not break when substring is at the start" do
-    substring = LongestCommonSubstring.length_for_pair("HELLObb", "asdfHELLOfoobar")
-    expect(substring).to eq("HELLO".length)
-  end
-
-  it "does not break when substring is at the end" do
-    substring = LongestCommonSubstring.length_for_pair("watwhutHELLO", "asdfHELLOfoobar")
-    expect(substring).to eq("HELLO".length)
-  end
-end
-
 describe PrivateKeys do
   it "knows when your clipboard is similar to one of the private keys" do
     PrivateKeys::Filesystem.stub(:all).and_return([
       "foo",
       "bar",
-      "zzzzzabcdefghjiklmnopqrstuvwxyzzzzz",
-      "baz",
+      "stuffstuffstuffstuffstuffabcdefghjiklmnopqrstuvwxyz\n-----END RSA PRIVATE KEY-----",
+      "baz"
     ])
     keys = PrivateKeys.new(26)
     expect(keys.similar?('abcdefghjiklmnopqrstuvwxyz')).to be_true
@@ -35,7 +18,7 @@ describe Main do
   it "replaces the clipboard if it is too close to the public key" do
     clipboard_contents = "badger badger badger badger mushroom mushroom"
     Clipboard.copy(clipboard_contents)
-    PrivateKeys::Filesystem.stub(:all).and_return([clipboard_contents])
+    PrivateKeys::Filesystem.stub(:all).and_return([clipboard_contents + PrivateKeys::KEY_END])
     Main.check_clipboard
     expect(Clipboard.paste).to eq(Main::CORRECTION_TEXT)
   end
